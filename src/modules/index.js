12a25adce0a42d53f00ee93dc3456dd4be246c67
Damir Mustafin <frenchthebread@gmail.com>
2016-08-13 19:35:58 +0300

    modules

---
+++ src/modules/index.js
@@ -0,0 +1,33 @@
+export const start = {
+  name: 'default',
+  keywords: [
+    '*'
+  ],
+  run () {
+    console.log('Default module called!');
+    return 'Default module called!';
+  }
+}
+
+export const greet = {
+  name: 'greet',
+  keywords: [
+    'hi',
+    'hello'
+  ],
+  run () {
+    console.log('Hello module is called');
+    return 'Hello';
+  }
+}
+
+export const buy = {
+  name: 'buy',
+  keywords: [
+    'buy'
+  ],
+  run () {
+    console.log('Buy module is called');
+    return 'Buy';
+  }
+}
