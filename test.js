5d6d241e53393ab0548e0c8068be8a365d7daed8
Damir Mustafin <frenchthebread@gmail.com>
2017-03-22 02:05:05 +0200

    Add sample test with telegram leaf

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const Palm = require('./').default
 
+// const telegram = require('./config').telegram
+
+// const palm = new Palm({ talk: 'telegram', telegram })
+
 const palm = new Palm({ talk: 'cli' })
 
 palm.listen()
