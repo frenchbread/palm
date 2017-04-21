491d2e41573a1bc39cff516335810e611e7ba398
Damir Mustafin <frenchthebread@gmail.com>
2017-04-21 21:50:07 +0300

    Cleanup

--- src/cocos/index.js
+++ src/cocos/index.js
@@ -3,11 +3,9 @@
 import greet from './greet/coco'
 import time from './time/coco'
 import idk from './idk/coco'
-import apis from './apis/coco'
 
 export default {
 	greet,
 	time,
-	idk,
-	apis
+	idk
 }
